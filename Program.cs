5cc4b7e1329bfbf6aa6c6f0626a0a4965b5f0821
Smolnikov-Ivan <110120403+Smolnikov-Ivan@users.noreply.github.com>
2022-09-14 23:53:11 +0300

    Add file Program.cs
    This file contains the solution of the problem with the array already entered.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача: 
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+*/
+
+
+string[] array = { "hello", "2", "world", ":-)", "222" };
+Console.Write("[ ");
+void Print(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+}
+Print(array);
+Console.Write("]");
+Console.WriteLine();
+filteredArray(array);
+void filteredArray(string[] array)
+{
+    Console.Write("[ ");
+    string[] newarray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[i] = array[i];
+            Console.Write(newarray[i] + " ");
+        }
+    }
+}
+Console.Write("]");
